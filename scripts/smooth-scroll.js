d8548329f34cb84bebd2c3226ae30e83b055b6b9
reijjo <teemu.aitomeri@gmail.com>
2023-05-08 22:27:21 +0300

    Added some links

--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -10,3 +10,19 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+function scrollToElement(elementId) {
+  const element = document.getElementById(elementId);
+  element.scrollIntoView({ behavior: "smooth" });
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  var links = document.querySelectorAll('#work a[href^="#"]');
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      var target = document.querySelector(this.getAttribute("href"));
+      target.scrollIntoView({ behavior: "smooth", block: "start" });
+    });
+  }
+});
